17e4a31c98b63c5e402f277d190c4513e1a3d3fb
Teetuu2 <teetuuwork@gmail.com>
2021-11-20 23:20:16 -0600

    Small changes to app.js

--- server/app.js
+++ server/app.js
@@ -8,3 +8,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
 
+app.post('/insert', (request, resposne) => {
+
+});
+
+app.get('/getAll', (request, response) => {
+    console.log('Reading Request');
+});
+
+app.listen(process.env.PORT, () => console.log('Successfully reached app.js'))
