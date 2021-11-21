5af256201172f2009879753f2801b28f0cabe205
Teetuu2 <teetuuwork@gmail.com>
2021-11-21 14:38:47 -0600

    Moved server modules into server folder and added html/css to client folder

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
+const { response } = require("express");
 
+document.addEventListener('DOMContentLoaded', function() {
 });
 
 function loadTables(data) {
--- server/app.js
+++ server/app.js
@@ -8,12 +8,15 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
 
+//Create
 app.post('/insert', (request, resposne) => {
 
 });
 
+//Read
 app.get('/getAll', (request, response) => {
     console.log('Reading Request');
 });
 
-app.listen(process.env.PORT, () => console.log('Successfully reached app.js'))
+//Connecting
+app.listen(process.env.PORT, () => console.log('Successfully reached app.js'));
